a145f34090d7ef48645b5e796d524bdb51edfd95
Matteo Sandrin <sandrin98@gmail.com>
2025-12-19 18:39:11 -0500

    Configure for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   build: {
     outDir: "dist",
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.png"],
+      manifest: {
+        name: "Crunch QR",
+        short_name: "Crunch QR",
+        description: "Generate QR codes for Crunch Gym check-in",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            src: "/img/icon.png",
+            purpose: "maskable any",
+            type: "image/png",
+            sizes: "any",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
